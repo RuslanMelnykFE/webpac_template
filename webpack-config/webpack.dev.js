98cf3b22fd4adba9b7488cefc59429dce7c0b13a
RuslanMelnykFE <ruslan_melnyk@ukr.net>
2020-04-19 14:52:23 +0300

    added hot module replacement

--- webpack-config/webpack.dev.js
+++ webpack-config/webpack.dev.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
@@ -13,9 +14,13 @@ module.exports = merge(common, {
   },
   devtool: 'inline-sourse-map',
   // wath: true,
+  plugins: [
+    new webpack.HotModuleReplacementPlugin({})
+  ],
   devServer: {
     contentBase: path.join(__dirname, '../dist'),
     hot: true,
+    inline: true,
     port: 9090
   }
 });
